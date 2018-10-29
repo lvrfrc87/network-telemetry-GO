6b5f5b490265f6a1e9033580533a117029f2823b
federico.olivieri <federico.olivieri@ticketmaster.co.uk>
2018-10-29 22:38:13 +0000

    update

--- ping.go
+++ ping.go
@@ -4,48 +4,55 @@ import (
     "bytes"
     "fmt"
     "golang.org/x/crypto/ssh"
-    "io"
     "io/ioutil"
-    "os"
+    "log"
 )
 
 func main() {
   cmd := "ping -c 1 8.8.8.8"
-  var hosts []string {
+  hosts := []string {
     "ec2-3-8-77-175.eu-west-2.compute.amazonaws.com",
   }
   results := make(chan string, 10)
 
   // read the private key
-  key, err := ioutil.ReadFile("~/Downloads/olivierif.pem")
+  key, err := ioutil.ReadFile("/Users/federicoolivieri/Downloads/olivierif.pem")
   if err != nil {
     log.Fatalf("unable to read private key: %v", err)
     }
   // parse the prive key and create a ssh caller
-  signer, err := ssh.ParsePrivateKey(key)
+  singer, err := ssh.ParsePrivateKey(key)
   if err != nil {
       log.Fatalf("unable to parse private key: %v", err)
   }
-  var hostKey ssh.PublicKey
   config := &ssh.ClientConfig{
     Auth: []ssh.AuthMethod{
       ssh.PublicKeys(singer),},
+    // var hostKey ssh.PublicKey
+    // HostKeyCallback: ssh.FixedHostKey(hostKey)
+    HostKeyCallback: ssh.InsecureIgnoreHostKey(),
     }
-    HostKeyCallback: ssh.FixedHostKey(hostKey),
 
-  for _, hostname := range host {
+  for _, hostname := range hosts {
     go func(hostname string) {
-      result <- executeCmd(cmd, hostname, config)
+      results <- executeCmd(cmd, hostname, config)
     }(hostname)
   }
+
+  for i := 0; i < len(hosts); i++ {
+      select {
+      case res := <-results:
+          fmt.Print(res)
+        }
+    }
 }
 
 func executeCmd(command, hostname string, config *ssh.ClientConfig) string {
-  client, err := ssh.Dial("tcp", hostname, config)
+  client, err := ssh.Dial("tcp", "22", hostname, config)
   if err != nil {
       log.Fatalf("unable to connect: %v", err)
   }
-  session, err := conn.NewSession()
+  session, err := client.NewSession()
   if err != nil {
       log.Fatal("Failed to create session: ", err)
   }
