8888c84977dc4362ccf88bf61cf437b0d9ffacb9
federico.olivieri <federico.olivieri@ticketmaster.co.uk>
2018-11-13 12:14:34 +0000

    split influx func

--- src/ping/main.go
+++ src/ping/main.go
@@ -11,30 +11,33 @@ import (
 )
 
 func main() {
-	// while loop
 	for {
-		// loop through target list
 		for _, ip := range yaml.YamlReader().Targets {
 			// go routine
 			go influxdb(json.JsonBody(ping.RunPing(ip), yaml.YamlReader().Region))
 		}
-		time.Sleep(1 * time.Second)
 	}
 }
 
 func influxdb(r json.Body) {
 	// Create a new HTTPClient
-  dblist := []string{"localhost",}
-  for _, db := range dblist {
-  	c, err := client.NewHTTPClient(client.HTTPConfig{
-  		Addr:     fmt.Sprintf("http://%s:8086", db),
-  		Username: "root",
-  		Password: "supersecretpassword",
-  	})
-  	if err != nil {
-  		log.Printf("Failed to create %s client session: %v", db, err)
-  	}
-  	defer c.Close()
+	dbList := []string{"app1.net.awsieprod2.linsys.tmcs", "db1.telemetry.netams1.netsys.tmcs",}
+	for _, db := range dbList {
+		// go routine
+		go writeDb(db, r)
+  }
+}
+
+func writeDb(db string, r json.Body) {
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr:     fmt.Sprintf("http://%s:8086", db),
+		Username: "root",
+		Password: "supersecretpassword",
+	})
+	if err != nil {
+		log.Fatalf("Failed to create %s client session: %v", db, err)
+	}
+	defer c.Close()
 	// Create a new point batch
 	bp, _ := client.NewBatchPoints(client.BatchPointsConfig{
 		Database:  "network_telemetry",
@@ -54,8 +57,8 @@ func influxdb(r json.Body) {
 	pt, _ := client.NewPoint("ping_rtt_go", tags, fields, time.Now())
 	bp.AddPoint(pt)
 	// Write the batch
-  c.Write(bp)
+	c.Write(bp)
 	// Close client resources
-  c.Close();
-  }
+	c.Close();
+	return
 }
